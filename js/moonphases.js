ee4a0af5f0bd1158fa2d5c3cab85581a9cc742cc
sadnixon <askspencerhill@berkeley.edu>
2021-11-05 15:00:03 -0700

    moon pics

--- js/moonphases.js
+++ js/moonphases.js
@@ -9,23 +9,25 @@
     const currentdays = currentfrac * lunardays;
 
     const phases = [
-        ["new", 0, 1],
-        ["waxing crescent", 1, 6.38264692644],
-        ["first quarter", 6.38264692644, 8.38264692644],
-        ["waxing gibbous", 8.38264692644, 13.76529385288],
-        ["full", 13.76529385288, 15.76529385288],
-        ["waning gibbous", 15.76529385288, 21.14794077932],
-        ["last quarter", 21.14794077932, 23.14794077932],
-        ["waning crescent", 23.14794077932, 28.53058770576],
-        ["new", 28.53058770576, 29.53058770576],
+        ["New", 0, 1,"new.jpeg"],
+        ["Waxing Crescent", 1, 6.38264692644,"waxing_crescent.jpeg"],
+        ["First Quarter", 6.38264692644, 8.38264692644,"first_quarter.jpeg"],
+        ["Waxing Gibbous", 8.38264692644, 13.76529385288,"waxing_gibbous.jpeg"],
+        ["Full", 13.76529385288, 15.76529385288,"full.jpeg"],
+        ["Waning Gibbous", 15.76529385288, 21.14794077932,"waning_gibbous.jpeg"],
+        ["Last Quarter", 21.14794077932, 23.14794077932,"last_quarter.png"],
+        ["Waning Crescent", 23.14794077932, 28.53058770576,"waning_crescent.jpeg"],
+        ["New", 28.53058770576, 29.53058770576,"new.jpeg"],
     ];
 
     let thephase
     for (let i=0; i<9; i++ ) {
         if ((currentdays >= phases[i][1]) && (currentdays <= phases[i][2])) {
             thephase = phases[i][0];
+            phasepic = phases[i][3];
             break
         }
     }
     $("#moon").append('<p>The Moon Phase is ' + thephase + '</p>')
+    $("#moon").append('<img src="https://sadnixon.github.io/meishapp/moons/' + phasepic + '" width=400" height="400"/>')
 })();
